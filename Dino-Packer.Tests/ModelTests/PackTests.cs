5b0d78be2375bbc916579a4fef455c778b07bd24
Christine Frank and nikhil batta <christine@>
2019-10-09 08:38:25 -0700

    Add separate tests for models

---
+++ Dino-Packer.Tests/ModelTests/PackTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using DinoPacker.Models;
+
+namespace DinoPacker.Tests
+{
+
+    [TestClass]
+    public class PackTest
+    {
+        [TestMethod]
+        public void Pack_CreateInstance_Object()
+        {
+            Pack pack = new Pack();
+            Assert
+        }
+    }
